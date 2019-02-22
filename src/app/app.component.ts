985317a35e641a8c5100f71a506fa99103d430f5
Rafael Quines <rafael.quines@totvs.com.br>
2019-02-22 09:26:59 -0300

    dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web';
+  workers: any[] = [];
+  constructor(private socket: Socket) {
+    socket.emit('register', 'WEB');
+    socket.on('list_workers', (listWorkers) => {
+      console.log('Received list workers: ', listWorkers);
+      Object.keys(listWorkers).forEach((i) => {
+        this.workers.push(listWorkers[i]);
+      });
+      // this.workers = listWorkers;
+    });
+    socket.on('worker_disconnect', (worker: any) => {
+      console.log('Received worker_disconnect: ', worker.id);
+      this.workers = this.workers.filter((f) => f.id !== worker.id);
+    });
+    socket.on('worker_added', (worker: any) => {
+      console.log('Received worker_added: ', worker.id);
+      this.workers.push(worker);
+    });
+
+    socket.on('update_status', (worker: any) => {
+      console.log('update_status');
+      const myWorker = this.workers.find((f) => f.id === worker.id);
+      myWorker.status = worker.status;
+    });
+
+    socket.on('init_sync', (worker: any) => {
+      console.log('init_sync');
+      const myWorker = this.workers.find((f) => f.id === worker.id);
+      myWorker.shopId = worker.shopId;
+    });
+
+    socket.on('update_sync', (worker: any) => {
+      console.log('update_sync');
+      const myWorker = this.workers.find((f) => f.id === worker.id);
+      myWorker.progress = worker.progress;
+      myWorker.shopId = worker.shopId;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config: SocketIoConfig = { url: 'ws://localhost:5000', options: {} };
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
